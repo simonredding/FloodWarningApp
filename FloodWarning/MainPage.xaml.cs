2994e8da68ebf222a39995cf6348a3d0900a1d86
simonneb <simon.nebesnuick@gmail.com>
2016-01-10 19:21:24 +0000

    First version

---
+++ FloodWarning/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FloodWarning
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+          /* This code is for the use of Geolocation in the app.  Not currently in use
+            
+            var position = await LocationManager.GetPosition();
+            
+            var lat = position.Coordinate.Latitude;
+            var lon = position.Coordinate.Longitude;
+
+            RootObject myFlooding = 
+                await OpenFloodWarningProxy.GetFloodWarnings(
+                        lat,
+                        lon);
+
+            */            
+            
+            //This is the call to the API - anything in the brackets must be the same type as specified in the GetFloodWarnings code. For example a county of River.
+
+            RootObject myFlooding = await OpenFloodWarningProxy.GetFloodWarnings("London");
+                     
+            //This is what is returned. The bracketted item is the specific TA in the returned list. 
+
+            ResultFloodWarningsMessage.Text = myFlooding.items[5].message;
+
+            ResultFloodWarningsTimeUpdated.Text = myFlooding.items[5].timeMessageChanged;
+
+            ResultFloodWarningsTACode.Text = myFlooding.items[5].floodAreaID;
+            
+        }
+
+    }
+}
---
+++ FloodWarning/OpenFloodWarningProxy.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodWarning
+{
+
+    //Creating the call to the API
+
+    public class OpenFloodWarningProxy
+    {
+        //Note this the string must be same as the URL query and the info in the Mainpage.xaml.cs code too.
+
+        public async static Task<RootObject> GetFloodWarnings(string county)
+        {
+            var http = new HttpClient();
+
+            //Note that the url has a search value. This can be altered, but you must also alter the Mainpage.xaml.cs code too.
+
+            var response = await http.GetAsync("http://environment.data.gov.uk/flood-monitoring/id/floods?county=London");
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            var serializer = new DataContractJsonSerializer(typeof(RootObject));
+
+            var ms = new MemoryStream(Encoding.UTF8.GetBytes(result));
+
+            var data = (RootObject)serializer.ReadObject(ms);
+
+            return data;
+        }
+    }
+
+    //List of the API returns in c sharp. Remember to blank out any calls that return errors.
+
+    [DataContract]
+    public class Meta
+    {
+        [DataMember]
+        public string publisher { get; set; }
+
+        [DataMember]
+        public string licence { get; set; }
+
+        [DataMember]
+        public string documentation { get; set; }
+
+        [DataMember]
+        public string version { get; set; }
+
+        [DataMember]
+        public string comment { get; set; }
+
+        [DataMember]
+        public List<string> hasFormat { get; set; }
+    }
+
+    [DataContract]
+    public class FloodArea
+    {
+        /*
+        [DataMember]
+        public string __invalid_name__@id { get; set; }
+        */
+        [DataMember]
+        public string county { get; set; }
+
+        [DataMember]
+        public string notation { get; set; }
+
+        [DataMember]
+        public string polygon { get; set; }
+
+        [DataMember]
+        public string riverOrSea { get; set; }
+    }
+
+    [DataContract]
+    public class Item
+    {
+        /*
+        [DataMember]
+        public string __invalid_name__@id { get; set; }
+        */
+        [DataMember]
+        public string description { get; set; }
+
+        [DataMember]
+        public string eaAreaName { get; set; }
+
+        [DataMember]
+        public string eaRegionName { get; set; }
+
+        [DataMember]
+        public FloodArea floodArea { get; set; }
+
+        [DataMember]
+        public string floodAreaID { get; set; }
+
+        [DataMember]
+        public bool isTidal { get; set; }
+
+        [DataMember]
+        public string message { get; set; }
+
+        [DataMember]
+        public string severity { get; set; }
+
+        [DataMember]
+        public int severityLevel { get; set; }
+
+        [DataMember]
+        public string timeMessageChanged { get; set; }
+
+        [DataMember]
+        public string timeRaised { get; set; }
+
+        [DataMember]
+        public string timeSeverityChanged { get; set; }
+    }
+
+    [DataContract]
+    public class RootObject
+    {
+        /*
+        [DataMember]
+        public string __invalid_name__@context { get; set; }
+        */
+        [DataMember]
+        public Meta meta { get; set; }
+
+        [DataMember]
+        public List<Item> items { get; set; }
+    }
+}
